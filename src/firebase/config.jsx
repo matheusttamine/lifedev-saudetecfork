893fec74f4a5dc4189ed52a046eb8eb6d1775c17
VitorBZS <bonilhazanattasilva@gmail.com>
2025-03-11 12:34:50 -0300

    Configuração das variáveis do FireBase

---
+++ src/firebase/config.jsx
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import{ getFireStore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_4y-nK5V3nag6KyNy9KvHfVuX-b4n3h0",
+  authDomain: "lifedev-bachiega.firebaseapp.com",
+  projectId: "lifedev-bachiega",
+  storageBucket: "lifedev-bachiega.firebasestorage.app",
+  messagingSenderId: "729654581957",
+  appId: "1:729654581957:web:3383838b69635aacce6eac",
+  measurementId: "G-DCQNRCXQ8V"
+}
+
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+const db = getFireStore(app)
+
+export {db}
